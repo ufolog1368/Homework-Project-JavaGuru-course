a0272e777249334334d3f0c0a2bb5eb9f3c62487
igor <ufolog1368@gmail.com>
2020-08-06 22:22:32 +0300

    test

--- src/main/java/by/HW1JavaGuru/App.java
+++ src/main/java/by/HW1JavaGuru/App.java
@@ -5,12 +5,8 @@ import by.HW1JavaGuru.bean.Person;
 
 public class App {
     public static void main(String[] args) {
-        Person person = new Person();
-        person.setAge(17);
-        person.setMame("Igor");
 
-        PersonService personService = new PersonService(person);
-        System.out.println("personService.ifAult() = " + personService.ifAult());
+
 
     }
 }
